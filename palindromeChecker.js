6af6977e8282f4d5a8a8d135119886987fca2697
Jorge Cruz <100860547+jdcruz83@users.noreply.github.com>
2022-09-30 11:27:59 +0100

    update

--- palindromeChecker.js
+++ palindromeChecker.js
@@ -5,7 +5,39 @@ We'll pass strings with varying formats, such as racecar, RaceCar, and race CAR 
 We'll also pass strings with special symbols, such as 2A3*3a2, 2A3 3a2, and 2_A3*3#A2.*/
 
 function palindrome(str) {
-    return true;
+    //RegExp for matching characters
+    const regex = /[a-zA-Z0-9]/ig;
+    
+    //delete non-alphanumeric characters
+    const myArr = str.match(regex);
+    const myStr = myArr.join("").toLowerCase();
+
+    //reverse array && turn to lower case
+    const reverseArr = myArr.reverse();
+    const myReverseStr = reverseArr.join("").toLowerCase();
+
+    //compare with original string
+    if (myStr === myReverseStr) {
+        return true;
+    } else {
+        return false;
+    };
 }
 
 palindrome("eye");
+palindrome("_eye");
+palindrome("race car");
+palindrome("race car"); //should return true.
+palindrome("not a palindrome"); //should return false.
+palindrome("A man, a plan, a canal. Panama"); //should return true.
+palindrome("never odd or even"); //should return true.
+palindrome("nope"); //should return false.
+palindrome("almostomla"); //should return false.
+palindrome("My age is 0, 0 si ega ym."); //should return true.
+palindrome("1 eye for of 1 eye."); //should return false.
+palindrome("0_0 (: /-\ :) 0-0"); //should return true.
+palindrome("five|\_/|four"); //should return false.
+
+console.log(palindrome("not a palindrome"));
+console.log(palindrome("0_0 (: /-\ :) 0-0"));
+console.log(palindrome("never odd or even"));
